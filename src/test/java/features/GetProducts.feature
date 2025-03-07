ee2a6d4adaae126d742d7b564c5e67dae6e959ad
TazoPsuturi <tazopsuturi33@gmail.com>
2025-03-07 17:52:39 +0400

    added rate check scenario and procedure for procuct

--- src/test/java/features/GetProducts.feature
+++ src/test/java/features/GetProducts.feature
@@ -4,3 +4,12 @@ Feature: Get all products from API
     Given I hit the url of get products api endpoint
     When I pass the url in the request
     Then I receive the response code as 200
+
+
+  Scenario Outline: Verify the rate of the first product is correct
+    Given I hit the url of get products api endpoint
+    When I pass the url in the request
+    Then I verify that the rate of first product is <FirstProductRate>
+    Examples:
+      | FirstProductRate |
+      | 3.9              |
--- src/test/java/stepdefinitions/Products.java
+++ src/test/java/stepdefinitions/Products.java
@@ -4,7 +4,9 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
 import org.testng.Assert;
 import io.restassured.specification.RequestSpecification;
 
@@ -14,6 +16,7 @@ public class Products {
 	public RequestSpecification request;
 	public Response response;
 	public int ResponseCode;
+	//public ResponseBody body;
 	
 	
 	@Given("I hit the url of get products api endpoint")
@@ -31,5 +34,18 @@ public class Products {
 	public void i_receive_the_response_code_as(Integer int1) {
 		ResponseCode = response.getStatusCode();
 		Assert.assertEquals(ResponseCode, 200);
+	}
+	
+	@Then("I verify that the rate of first product is {}")
+	public void i_verify_that_the_rate_of_first_product_is(String rate) {
+		ResponseBody body = response.getBody();
+		
+		String responseBody = body.asString();
+		
+		JsonPath jsonPath = response.jsonPath();
+		
+		String rateOfProduct = jsonPath.getJsonObject("rating[0].rate").toString();
+		
+		Assert.assertEquals(rate, rateOfProduct);
 	}
 }
